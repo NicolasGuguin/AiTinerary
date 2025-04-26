1f8871f362b9882bddaf80d89ade05a98ad18db3
Nicolas Guguin <n.guguin@gmail.com>
2025-04-27 01:08:40 +0200

    redirection link vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/', // ➔ ⚡ Ajout obligatoire pour un fonctionnement SPA propre sur Vercel
   plugins: [react()],
   server: {
     port: 5173
